ab76ae15d94f94d0bfd66f275383bb6e60fc0459
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-02 16:37:08 +0100

    backend-test

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Origin: http://127.0.0.1:3000");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-require __DIR__ . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Ramsey\Uuid\Uuid;
 
