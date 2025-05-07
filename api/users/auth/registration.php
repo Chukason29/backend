bdf868e088f5f6dd345b1403679e35d827006ef0
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-07 21:59:12 +0100

    added php mailer

--- api/users/auth/registration.php
+++ api/users/auth/registration.php
@@ -4,12 +4,22 @@ if (!isset($data['name'], $data['email'], $data['password'])) {
     respond(["status" => "false",'message' => 'All fields are required'], 400);
 }
 
+#TODO check if email is valid
 if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
     respond(["status" => "false", 'message' => 'Invalid email address'], 400);
 }
 if (empty($data["name"])) {
     respond(["status" => "false", 'message' => 'Name is required'], 400);
 }
+
+if (empty($data['password'])) {
+    respond(["status" => "false", 'message' => 'Password is required'], 400);
+}
+
+if (strlen($data['password']) < 8) {
+    respond(["status" => "false", 'message' => 'Password must be at least 8 characters long'], 400);
+}
+
 $name = strtolower(sanitizeInput($data["name"]));
 $email = strtolower(sanitizeInput($data["email"]));
 
