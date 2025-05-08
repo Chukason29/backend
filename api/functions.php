1c7e1442a97ab2a88c1329b815113a9c4f8473ae
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-08 08:21:06 +0100

    email reg sender

--- api/functions.php
+++ api/functions.php
@@ -97,15 +97,15 @@ function sendHTMLEmail($toEmail, $toName, $verificationLink, $myTemplate) {
     try {
         // SMTP Configuration
         $mail->isSMTP();
-        $mail->Host = "smtp.gmail.com"; // SMTP server (e.g., hirepurchase.ng)
+        $mail->Host = "hirepurchase.ng"; // SMTP server (e.g., hirepurchase.ng)
         $mail->SMTPAuth = true;
-        $mail->Username = 'support@trendsaf.co'; // SMTP Username
-        $mail->Password = $config['mail']['password']; // SMTP Password
+        $mail->Username = 'admin@hirepurchase.ng'; // SMTP Username
+        $mail->Password = "sanctifier1994.com"; // SMTP Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Encryption
         $mail->Port = 587; // SMTP Port (Gmail: 587, Outlook: 587, SSL: 465)
 
         // Sender and Recipient
-        $mail->setFrom('support@trendsaf.co', 'Trendsaf BaseFood');
+        $mail->setFrom('admin@hirepurchase.ng', 'Trendsaf BaseFood');
         $mail->addAddress($toEmail, $toName);
         #$mail->addReplyTo('admin@hirepurchase.ng', 'Support');
 
