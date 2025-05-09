8fe47b044d2bab3ecbbd41fd246333cd38363286
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-09 09:53:50 +0100

    url reset

--- api/users/auth/registration.php
+++ api/users/auth/registration.php
@@ -28,6 +28,8 @@ $hashedPassword = password_hash($data['password'], PASSWORD_BCRYPT);
 /*if (emailExists($pdo, $email)) {
     respond(["status" => "false", 'message' => 'Account already exists'], 400);
 }*/
+$token = generateTimedToken($email, 86400); //expires in 24hours after creation
+$verifyLink = $config['url']['BASE_URL'].'/api/users/auth/verify?token='.$token;
 if (sendHTMLEmail($email, $name, $verifyLink, dirname(__DIR__, 2)."/templates/email_verification.html")) {
         respond(["status" => "success", "message" => "Registration successful, link sent to your email"]);
         exit;
@@ -35,8 +37,7 @@ if (sendHTMLEmail($email, $name, $verifyLink, dirname(__DIR__, 2)."/templates/em
     respond(["status" => "error", "message" => "Registration is unsuccessful"]);   
 } exit;
 #TODO ==> Create a timed token based on the user's email and attach to the base url
-$token = generateTimedToken($email, 86400); //expires in 24hours after creation
-$verifyLink = $config['url']['BASE_URL'].'/api/users/auth/verify?token='.$token;
+
 
 
 try {
