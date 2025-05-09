a3155683b7306602f219a7e554fd6b09d8d59424
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-09 13:29:36 +0100

    email resolve

--- api/users/auth/verify.php
+++ api/users/auth/verify.php
@@ -1,3 +1,18 @@
 <?php
+
+    #TODO ==> check if the token is valid and not expired
+    $sql = "SELECT * FROM link_token WHERE token = :token AND check_column = false AND created_at > NOW() - INTERVAL 1 DAY";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':token' => $verify_token]);
+    $tokenData = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$tokenData) {
+        respond(["error" => "Invalid or expired token"], 400);
+    }
+    respond(["status" => "success","token" => $tokenData], 200);
+    #TODO ==> check if the token is already used or not
+
+    #TODO ==> If the token is valid and not expired, update the token table with true as the check column
+
+    #TODO ==> If the token is valid and not expired, update the user table with true as the check column
     respond(["token" => $verify_token], 200);
     exit;
