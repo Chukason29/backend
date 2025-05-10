537b4327f4f71a86206855c1eb0540d141527eda
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-10 20:32:56 +0100

    auth updates

--- api/super_admin/add_roles.php
+++ api/super_admin/add_roles.php
@@ -13,8 +13,31 @@ function respond($data, $code = 200) {
 }
 $uuid = Uuid::uuid4()->toString();
 
-respond(["status" => "success",'message' => $uuid], 400);
-if (!isset($data['name'], $data['email'], $data['password'])) {
+if (!isset($data['name'])) {
     respond(["status" => "false",'message' => 'All fields are required'], 400);
+}
+try {
+    $pdo->beginTransaction();
+    $stmt1 = $pdo->prepare("INSERT INTO roles ( id, name ) VALUES (:id, :name)");
+    $stmt1->execute([
+        ':name' => $name,
+        ':id' => $uuid
+    ]);
+
+     #TODO commit data to database and send link to email address
+    if ($pdo->commit() ){
+        respond(["status" => "success", "message" => "role added successfully"], 200);
+        exit;
+    }else{
+        respond(["status" => "error", "message" => "Unsuccessful"], 200);   
+    }
+
+respond(['message' => 'User registered successfully'], 201);
+} catch (PDOException $e) {
+    $pdo->rollBack();
+    respond(['error' => 'Database error: ' . $e->getMessage()], 500);
+} catch (Exception $e) {
+    $pdo->rollBack();
+    respond(['error' => 'Error: ' . $e->getMessage()], 500);
 }
 
