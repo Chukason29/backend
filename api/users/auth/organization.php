bddad3ff060238350155cdae700926e41c00c7ee
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-11 19:43:25 +0100

    add subscriptions

--- api/users/auth/organization.php
+++ api/users/auth/organization.php
@@ -13,7 +13,8 @@
     }
     #TODO ==> Make sure email and password is inputted
     if ($_SESSION['user_id'] ?? null) {
-        # code...
+        respond(["status" => "success", 'user_id' => $_SESSION['user_id']], 400);
+        exit;
     }
     if (!isset($data['email'], $data['password'])) {
         respond(["status" => "false",'message' => 'All fields are required'], 400);
