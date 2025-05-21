7ce42361bd799df3bda8a0f25ee1ee4020e8c9b4
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-21 18:27:39 +0100

    Port number CORS

--- api/routes.php
+++ api/routes.php
@@ -10,6 +10,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Set CORS headers
 header("Access-Control-Allow-Origin: http://127.0.0.1:3001");
+header("Access-Control-Allow-Origin: http://localhost:3001");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 //file_put_contents('log.txt', "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . PHP_EOL, FILE_APPEND);
