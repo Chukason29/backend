a2e28cb8041477cd09f715db14bef723c5135334
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-25 08:18:44 +0100

    login check

--- api/users/auth/verify.php
+++ api/users/auth/verify.php
@@ -2,13 +2,6 @@
 
     $data = json_decode(file_get_contents('php://input'), true);
 
-    // Respond helper
-    function respond($data, $code = 200) {
-        http_response_code($code);
-        header('Content-Type: application/json');
-        echo json_encode($data);
-        exit;
-    }
     
     #TODO ==> check if the token is valid and not expired
     $email = getEmailFromToken($verify_token);
