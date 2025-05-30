53e3af6f8f52b27d39445e8bad9ce42cad455e40
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-30 14:42:33 +0100

    Permissions

--- config.php
+++ config.php
@@ -16,10 +16,10 @@ session_set_cookie_params([
     'samesite' => 'None',    // Helps prevent CSRF (use 'Strict' for tighter security)
 ]);
 
-ini_set('session.use_strict_mode', 0);        // Reject uninitialized session IDs
+ini_set('session.use_strict_mode', 1);        // Reject uninitialized session IDs
 ini_set('session.use_only_cookies', 1);       // Prevent session IDs in URLs
-ini_set('session.cookie_secure', 0);          // Force HTTPS
-ini_set('session.cookie_httponly', 0);        // Prevent JS access
+ini_set('session.cookie_secure', 1);          // Force HTTPS
+ini_set('session.cookie_httponly', 1);        // Prevent JS access
 
 return [
     //
