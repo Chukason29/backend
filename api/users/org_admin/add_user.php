c062a77d07e3550b9e1a287cebbe4a4f78e928b0
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-05-30 08:50:05 +0100

    change sessions

--- api/users/org_admin/add_user.php
+++ api/users/org_admin/add_user.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__, 2) . '/tokenization.php';
+require_once dirname(__DIR__, 1) . '/tokenization.php';
 
 
 respond(['status' => 'error', 'message' => $decoded_token], 200);
