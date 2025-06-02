3983dc62e8a9152a26926a9bc48706b506195bf7
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-06-02 14:09:46 +0100

    add user

--- api/functions.php
+++ api/functions.php
@@ -171,7 +171,7 @@ function decryptUserId($encryptedValue, $encryptionKey) {
     return openssl_decrypt($ciphertext, 'AES-256-CBC', $encryptionKey, 0, $iv);
 }
 
-function generateAccessToken($userId, $role, $name, $email, $organization_id, $secret, $expiresIn = 120) {
+function generateAccessToken($userId, $role, $name, $email, $organization_id, $secret, $expiresIn = 3660 * 3) {
     $issuedAt = time();
     $payload = [
         'iss' => 'trendsaf-api',
