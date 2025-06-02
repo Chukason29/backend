7a258c27de33da10ea2d331c3a7d0ba7b6913934
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-06-02 14:48:57 +0100

    add user issues

--- api/users/org_admin/add_user.php
+++ api/users/org_admin/add_user.php
@@ -1,6 +1,9 @@
 <?php
-
-require __DIR__ . '/../../tokenization.php';
+## tokenization.php is used for
+#checking if access token is valid or expired
+#decrypting the user id from the token
+# checking if user has access to the requested resource
+require __DIR__ . '/../../tokenization.php'; 
 
 respond(['status' => 'sucess', 'message' => "You can now add user"], 200);
 exit;
