2921b5664f8e1b13ccca7e8226dba33d5b885584
chukwuebuka <chukwuebukaalaegbu@gmail.com>
2025-06-02 16:32:15 +0100

    add user issues

--- api/users/org_admin/add_user.php
+++ api/users/org_admin/add_user.php
@@ -18,9 +18,25 @@ if (empty($data["name"])) {
     respond(["status" => "error", 'message' => 'Name is required'], 400);
 }
 #generate a random password 
-$password = bin2hex(random_bytes(4 )); // 16 characters long
+$password = bin2hex(random_bytes(4)); // 16 characters long
 
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 if (emailExists($pdo, $data['email'])) {
     respond(["status" => "error", 'message' => 'Account already exists'], 400);
+}
+$name = strtolower(sanitizeInput($data["name"]));
+$email = strtolower(sanitizeInput($data["email"]));
+
+try {
+    $pdo->beginTransaction();
+    $stmt1 = $pdo->prepare("INSERT INTO users ( name, email, password_hash, role_id) VALUES (:name, :email, :hashed_password, :role_id)");
+    $stmt1->execute([
+        ':name' => $name,
+        ':email' => $email,
+        ':hashed_password' => $hashedPassword, // Always hash passwords
+        ':role_id' => $role_id
+    ]);
+} catch (PDOException $e) {
+    $pdo->rollBack();
+    respond(["status" => "error", 'message' => 'Database error: ' . $e->getMessage()], 500);
 }
